5f8c540073f0fc8def7eb2eb874798e1329e4b3b
Arthur-vitology <arthur@vitology.be>
2026-05-22 11:56:31 +0200

    Update tiktok.js

--- api/tiktok.js
+++ api/tiktok.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const accessToken = process.env.TIKTOK_ACCESS_TOKEN;
   const advertiserId = process.env.TIKTOK_ADVERTISER_ID;
